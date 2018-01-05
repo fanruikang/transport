4f6d553319b124d072e9bda20a7f6b34e6f38885
fanruikang <1525297411@qq.com>
2018-01-05 17:32:27 +0800

    樊瑞康提交

--- Application/Home/Controller/SelfCenterController.class.php
+++ Application/Home/Controller/SelfCenterController.class.php
@@ -6,4 +6,30 @@ class SelfCenterController extends Controller {
     public function selfCenter(){
         $this->show();
     }
+    public function addCar(){
+        $id = $_POST["id"];
+        $weight =$_POST["weight"];
+        $length =$_POST["length"];
+        $width =$_POST["width"];
+
+        //$passwd = Request::instance()->post('passwd');
+
+        //添加车辆
+        $car = M("car"); // 实例化User对象
+        // 查找car.no 值为$_POST["id"]的用户数据
+        $condition["car_no"] =$id;
+        if($data = $car->where($condition)->find()){
+            $this->error("该车辆已被注册，请重试");
+        }
+        $newCar = [
+            'driver_id'=>$_SESSION["userId"],
+            'car_no'=> $id,
+            'car_weight'=>$weight,
+            'car_length'=>$length,
+            'car_width'=>$width
+        ];
+
+        $car->add($newCar);
+        $this->success("添加成功");
+    }
 }
